c6fcae7dc4206f2cc66d0768c136216914d32231
beiklive <1376896787@qq.com>
2022-08-18 23:42:37 +0800

    功能样式基本完成，细节后续添加
    Signed-off-by: beiklive <1376896787@qq.com>

--- StaticFile/res/index.js
+++ StaticFile/res/index.js
@@ -12,6 +12,20 @@ function ShowSettings() {
     settings.style.visibility = "visible";
 }
 
+function GetPage(params) {
+    var Box = document.getElementById("ChangeBox");
+    Box.innerHTML = "";
+    var data = JSON.parse(params);
+    for (var i = 0; i < data.length; i++) {
+        Box.innerHTML += "<a class=\"myitem\" href=" + data[i]['url'] + " target=\"_blank\">  \
+                        <p><i class=\"fa fa-fw " + data[i]['fo'] + "\"></i>&nbsp;"+data[i]['title']+"</p>\
+                    </a>"
+        console.log(data[i]['url']);
+        console.log(data[i]['title']);
+        console.log(data[i]['fo']);
+    }
+}
+
 
 function SendKey() {
     var key = document.getElementById("KeyInput").value;
@@ -23,21 +37,15 @@ function SendKey() {
     $.ajax({
         type: "GET",
         url: "http://api.beiklive.top/HomePage",
-        // data:"username=王方",
         data: { "key": key },
         async: false,
-        cache: true,
-        dataType: "jsonp",
-        jsonp: "jsoncallback",
-        contentType: "application/json;utf-8",
+        cache: false,
         success: function (backdata, status, xmlHttpRequest) {
             console.log("success");
             console.log(backdata);
             console.log(status);
             console.log(xmlHttpRequest);
-        },
-        error: function (msg) {
-            alert("错误内容" + msg)
+            GetPage(backdata);
         }
     });
 }
